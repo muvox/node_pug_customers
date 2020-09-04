ae2878edbc4c2aa5a2fe653e67fbd44f688a1e06
muvox <muvoksi@gmail.com>
2020-09-04 10:49:04 +0300

    Testing another thing

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser')
 
 const app = express()
 
+const port = 3000
+
 let customers = [
   {id: '1588323375416', firstname: 'John', lastname: 'Johnson', email: 'john@johnson.com', phone: '8233243'},
   {id: '1588323375417', firstname: 'Mary', lastname: 'Smith', email: 'mary@smith.com', phone: '6654113'},
@@ -29,6 +31,6 @@ app.post("/addcustomer", (req, res) => {
   res.redirect("/")
 })
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log(`Server is running on port ${port}.`)
 })
