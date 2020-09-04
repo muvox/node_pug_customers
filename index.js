28619a1913ab64aa744037f2d5a8a2e9c88425fb
muvox <muvoksi@gmail.com>
2020-09-04 10:44:55 +0300

    Changed port settings for heroku

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const bodyParser = require('body-parser')
 
 const app = express()
 
-const port = 3000
-
 let customers = [
   {id: '1588323375416', firstname: 'John', lastname: 'Johnson', email: 'john@johnson.com', phone: '8233243'},
   {id: '1588323375417', firstname: 'Mary', lastname: 'Smith', email: 'mary@smith.com', phone: '6654113'},
@@ -31,6 +29,6 @@ app.post("/addcustomer", (req, res) => {
   res.redirect("/")
 })
 
-app.listen(port, () => {
+app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${port}.`)
 })
